ed7aefcf189e17411702cedc69ece69cc2e1fd57
argos <levantedevelopments@gmail.com>
2019-10-06 13:11:56 +0200

    fix

--- lib/currencies.dart
+++ lib/currencies.dart
@@ -29,7 +29,40 @@ class Currency {
   }
 }
 
-enum Iso4217Code { usd, eur }
+enum Iso4217Code {
+  aud,
+  bgn,
+  brl,
+  cad,
+  chf,
+  czk,
+  dkk,
+  eur,
+  gbp,
+  hkd,
+  hrk,
+  huf,
+  idr,
+  ils,
+  inr,
+  isk,
+  jpy,
+  krw,
+  mxn,
+  myr,
+  nok,
+  nzd,
+  php,
+  pln,
+  ron,
+  rub,
+  sek,
+  sgd,
+  trk,
+  thb,
+  usd,
+  zar
+}
 
 /// Returns the Iso4217Code for a given iso code string.
 ///
@@ -47,43 +80,43 @@ Iso4217Code isoCodeFromStr(String isoCodeStr) {
 
 /// List of all Iso4217 currencies.
 final Map<Iso4217Code, Currency> currencies = <Iso4217Code, Currency>{
-  Iso4217Code.eur: const Currency(
+  Iso4217Code.aud: const Currency(
       isoCode: 'AUD',
       fullName: 'Australian Dollar',
       symbol: r'A$',
       minorUnit: 'cent',
       minorFraction: 100),
-  Iso4217Code.usd: const Currency(
+  Iso4217Code.bgn: const Currency(
       isoCode: 'BGN',
       fullName: 'Bulgarian Lev',
       symbol: r'лв',
       minorUnit: 'stotinki',
       minorFraction: 100),
-  Iso4217Code.usd: const Currency(
+  Iso4217Code.brl: const Currency(
       isoCode: 'BRL',
       fullName: 'Brazilian Real',
       symbol: r'R$',
       minorUnit: 'centavo',
       minorFraction: 100),
-  Iso4217Code.eur: const Currency(
+  Iso4217Code.cad: const Currency(
       isoCode: 'CAD',
       fullName: 'Canadian Dollar',
       symbol: r'C$',
       minorUnit: 'cent',
       minorFraction: 100),
-  Iso4217Code.eur: const Currency(
+  Iso4217Code.chf: const Currency(
       isoCode: 'CHF',
       fullName: 'Swiss Franc',
       symbol: r'CHF',
       minorUnit: 'rappen',
       minorFraction: 100),
-  Iso4217Code.eur: const Currency(
+  Iso4217Code.czk: const Currency(
       isoCode: 'CZK',
       fullName: 'Czech Koruna',
       symbol: r'Kč',
       minorUnit: 'haléru',
       minorFraction: 100),
-  Iso4217Code.eur: const Currency(
+  Iso4217Code.dkk: const Currency(
       isoCode: 'DKK',
       fullName: 'Danish Krone',
       symbol: r'Kr.',
@@ -95,133 +128,133 @@ final Map<Iso4217Code, Currency> currencies = <Iso4217Code, Currency>{
       symbol: r'€',
       minorUnit: 'cent',
       minorFraction: 100),
-  Iso4217Code.eur: const Currency(
+  Iso4217Code.gbp: const Currency(
       isoCode: 'GBP',
       fullName: 'British Pound',
       symbol: r'£',
       minorUnit: 'penny',
       minorFraction: 100),
-  Iso4217Code.eur: const Currency(
+  Iso4217Code.hkd: const Currency(
       isoCode: 'HKD',
       fullName: 'Hong Kong Dollar',
       symbol: r'HK$',
       minorUnit: 'cent',
       minorFraction: 100),
-  Iso4217Code.eur: const Currency(
+  Iso4217Code.hrk: const Currency(
       isoCode: 'HRK',
       fullName: 'Croatian Kuna',
       symbol: r'kn',
       minorUnit: 'lipa',
       minorFraction: 100),
-  Iso4217Code.eur: const Currency(
+  Iso4217Code.huf: const Currency(
       isoCode: 'HUF',
       fullName: 'Hungarian Forint',
       symbol: r'Ft',
       minorUnit: 'fillér',
       minorFraction: 100),
-  Iso4217Code.eur: const Currency(
+  Iso4217Code.idr: const Currency(
       isoCode: 'IDR',
       fullName: 'Indonesian Rupiah',
       symbol: r'Rp',
       minorUnit: 'sen',
       minorFraction: 100),
-  Iso4217Code.eur: const Currency(
+  Iso4217Code.ils: const Currency(
       isoCode: 'ILS',
       fullName: 'Israeli Shekel',
       symbol: r'₪',
       minorUnit: 'agorat',
       minorFraction: 100),
-  Iso4217Code.eur: const Currency(
+  Iso4217Code.inr: const Currency(
       isoCode: 'INR',
       fullName: 'Indian Rupee',
       symbol: r'₹',
       minorUnit: 'paisa',
       minorFraction: 100),
-  Iso4217Code.eur: const Currency(
+  Iso4217Code.isk: const Currency(
       isoCode: 'ISK',
       fullName: 'Icelandic Krona',
       symbol: r'íkr',
       minorUnit: '',
       minorFraction: 100),
-  Iso4217Code.usd: const Currency(
+  Iso4217Code.jpy: const Currency(
       isoCode: 'JPY',
       fullName: 'Japanese Yen',
       symbol: r'¥',
       minorUnit: 'sen',
       minorFraction: 100),
-  Iso4217Code.eur: const Currency(
+  Iso4217Code.krw: const Currency(
       isoCode: 'KRW',
       fullName: 'South Korean Won',
       symbol: r'₩',
       minorUnit: 'jeon',
       minorFraction: 100),
-  Iso4217Code.eur: const Currency(
+  Iso4217Code.mxn: const Currency(
       isoCode: 'MXN',
       fullName: 'Mexican Peso',
       symbol: r'Mex$',
       minorUnit: 'cent',
       minorFraction: 100),
-  Iso4217Code.eur: const Currency(
+  Iso4217Code.myr: const Currency(
       isoCode: 'MYR',
       fullName: 'Malaysian Ringgit',
       symbol: r'RM',
       minorUnit: 'sen',
       minorFraction: 100),
-  Iso4217Code.usd: const Currency(
+  Iso4217Code.nok: const Currency(
       isoCode: 'NOK',
       fullName: 'Norwegian Krone',
       symbol: r'kr.',
       minorUnit: 'øre',
       minorFraction: 100),
-  Iso4217Code.usd: const Currency(
+  Iso4217Code.nzd: const Currency(
       isoCode: 'NZD',
       fullName: 'New Zealand Dollar',
       symbol: r'NZ$',
       minorUnit: 'cent',
       minorFraction: 100),
-  Iso4217Code.usd: const Currency(
+  Iso4217Code.php: const Currency(
       isoCode: 'PHP',
       fullName: 'Philippine Peso',
       symbol: r'₱',
       minorUnit: 'sentimo',
       minorFraction: 100),
-  Iso4217Code.eur: const Currency(
+  Iso4217Code.pln: const Currency(
       isoCode: 'PLN',
       fullName: 'Polish Zloty',
       symbol: r'zł',
       minorUnit: 'grosze',
       minorFraction: 100),
-  Iso4217Code.eur: const Currency(
+  Iso4217Code.ron: const Currency(
       isoCode: 'RON',
       fullName: 'Romanian Leu',
       symbol: r'lei',
       minorUnit: 'bani',
       minorFraction: 100),
-  Iso4217Code.eur: const Currency(
+  Iso4217Code.rub: const Currency(
       isoCode: 'RUB',
       fullName: 'Russian Ruble',
       symbol: r'₽',
       minorUnit: 'kopek',
       minorFraction: 100),
-  Iso4217Code.eur: const Currency(
+  Iso4217Code.sek: const Currency(
       isoCode: 'SEK',
       fullName: 'Swedish Krona',
       symbol: r'kr',
       minorUnit: 'ören',
       minorFraction: 100),
-  Iso4217Code.usd: const Currency(
+  Iso4217Code.sgd: const Currency(
       isoCode: 'SGD',
       fullName: 'Singapore Dollar',
       symbol: r'S$',
       minorUnit: 'cent',
       minorFraction: 100),
-  Iso4217Code.eur: const Currency(
+  Iso4217Code.trk: const Currency(
       isoCode: 'TRY',
       fullName: 'Turkish Lira',
       symbol: r'₺',
       minorUnit: 'kuruş',
       minorFraction: 100),
-  Iso4217Code.eur: const Currency(
+  Iso4217Code.thb: const Currency(
       isoCode: 'THB',
       fullName: 'Thai Baht',
       symbol: r'฿',
@@ -233,7 +266,7 @@ final Map<Iso4217Code, Currency> currencies = <Iso4217Code, Currency>{
       symbol: r'$',
       minorUnit: 'cent',
       minorFraction: 100),
-  Iso4217Code.usd: const Currency(
+  Iso4217Code.zar: const Currency(
       isoCode: 'ZAR',
       fullName: 'South African Rand',
       symbol: r'R',
